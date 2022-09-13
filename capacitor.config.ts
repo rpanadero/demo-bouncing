20b5ad4bff5dd8354050fdb09e96e785922487af
Rubén Panadero <rpanadero@okode.com>
2022-09-13 13:51:54 +0200

    feat: add demo project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.okode.demo',
+  appName: 'demo-bouncing',
+  webDir: 'dist/demo-bouncing',
+  bundledWebRuntime: false
+};
+
+export default config;
